a5a8f119e621db145f51bfd80027df0b3a33aee1
Ben <quillaja@hotmail.com>
2019-07-27 10:00:12 -0700

    giving up on 2017/qual/c

--- 2017/qual/c/stalls.go
+++ 2017/qual/c/stalls.go
@@ -13,12 +13,13 @@ import (
 func main() {
 	var t int
 	fmt.Scanln(&t)
-	t = 15
+	// t = 15
 	var n, k uint64
 	for i := 0; i < t; i++ {
 		fmt.Scanln(&n, &k)
 		h, l := solve(n, k)
 		fmt.Printf("Case #%d: %d %d\n", i+1, h, l)
+		// fmt.Printf("Case #%d:(n=%d k=%d) %d %d\n", i+1, n, k, h, l)
 	}
 }
 
@@ -30,27 +31,76 @@ func main() {
 // isn't working for SOME cases when n is even (seems to work when n=1000...)
 func solve(n, k uint64) (h, l uint64) {
 	// level := int(math.Ceil(math.Log2(float64(k+1)) - 1))
-	level := int(math.Floor(math.Log2(float64(k))))
-	// fmt.Println(level)
-	h = n
-	l = n
-	for i := 0; i <= level; i++ {
-		// for i := 0; k >= uint64(math.Pow(2, float64(i))); i++ {
-		h = h / 2
-		if l > 0 {
-			l = (l - 1) / 2
+	// height := level(k)
+	// // fmt.Println(level)
+	// h, l = n/2, (n-1)/2
+	// if height == 0 {
+	// 	return
+	// }
+
+	// // go right
+	// solve(max(h, l), k-above(height))
+
+	// // go left
+	// solve(min(h, l), k-above(height))
+
+	todepth := level(k)
+	curdepth := uint64(0)
+	visited := uint64(0)
+	for curdepth <= todepth {
+		if n%2 == 0 && n > 0 {
+			h = n / 2
+			l = (n - 1) / 2
+		} else {
+			h = n / 2
+			l = n / 2
 		}
-		// if h > 0 {
-		// 	h = (h-1)/2 + 1
-		// }
-		// if l > 0 {
-		// 	l = (l - 1) / 2
-		// }
-		fmt.Println(h, l)
-		// if l > h {
-		// l, h = h, l
-		// }
+		// fmt.Println(h, l)
+
+		visited = above(curdepth)
+		curdepth++
+
+		// TODO: I don't have any idea how to figure out to "go right"
+		// or "go left". I think I'd have the problem solved if I could
+		// figure that out.
+		if (k-visited)%2 == 1 { // go right
+			// fmt.Println("went right")
+			n = max(h, l)
+			// k = k - 1
+		} else { // go left
+			// fmt.Println("went left")
+			n = min(h, l)
+			// k = k - 2
+		}
+		// fmt.Println(h, l, n, visited)
 	}
 
 	return
+}
+
+// level of kth (1,2,...) stall taken
+func level(k uint64) uint64 {
+	return uint64(math.Floor(math.Log2(float64(k))))
+}
+
+func max(a, b uint64) uint64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b uint64) uint64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func pow2(x uint64) uint64 {
+	return 1 << x
+}
+
+func above(h uint64) uint64 {
+	return pow2(h+1) - 1
 }
