b185acec607386f6987d84f047793586d74aab69
Ben <quillaja@hotmail.com>
2019-08-03 11:30:21 -0700

    solved 2017/qual/c small and large.
    solution worked on small, but large datasets included a trick number
    that broke the log2() func. Therefore I had to write a simple log2
    function for integers.

--- 2017/qual/c/stalls.go
+++ 2017/qual/c/stalls.go
@@ -2,12 +2,16 @@
 // first impression is that this is a binary tree type problem
 // but with N up to 10^18, this can't store "state" (it's an exabit
 // of memory at a minimum)
+//
+// last impressions: this was pretty hard for me, and thinking about it as a tree
+// didn't help--maybe hurt--although it did lead me to useful formulas regarding
+// binary trees. Inclusion of buggy numbers (see comments on math.Log2) is sneaky.
 
 package main
 
 import (
 	"fmt"
-	"math"
+	"math/bits"
 )
 
 func main() {
@@ -17,108 +21,35 @@ func main() {
 	var n, k int
 	for i := 0; i < t; i++ {
 		fmt.Scanln(&n, &k)
-		h, l := solve3(n, k)
+		h, l := solve(n, k)
 		fmt.Printf("Case #%d: %d %d\n", i+1, h, l)
 		// fmt.Printf("Case #%d:(n=%d k=%d) %d %d\n", i+1, n, k, h, l)
 	}
 }
 
-// seemed maybe closer.. but still wrong.
-func solve2(n, k int) (h, l int) {
-	h, l = n, n
-	// height := level(k)
-	kCurrent := 0
-	depth := 0
-	for kCurrent < k {
-		h = h/2 + h%2
-		l = l / 2
-		kLevel := pow2(depth)
-		if k-kCurrent < kLevel {
-			fmt.Println("k-kCurrent<kLevel = ", kCurrent, kLevel, k-kCurrent)
-			if kCurrent == int(pow2(depth+1)) {
-				fmt.Println("doing first in level")
-				h = h/2 + 1
-				l = l / 2
-			} else {
-				fmt.Println("doing rest in row")
-				h, l = h/2, l/2
-			}
-			kCurrent += kCurrent + kLevel
-		} else {
-			kCurrent -= kLevel
-		}
-		depth++
-		fmt.Println(h, l, kCurrent, depth)
-	}
-	return
-}
-
-// i don't know why this isn't working. it seems to work for all the
-// "explained" test cases (1-5) and some I made up.
-//
-// ... except it didnt: case 3 (n=6, k=2) was wrong. I'm getting (1,0), and the
-// answer is (1,1). On paper I'm getting this of course, but my little "formula"
-// isn't working for SOME cases when n is even (seems to work when n=1000...)
+// Recursive solution using some properties of binary trees (height, nodes at
+// a particular height, etc). It starts at the "bottom" (level of k), and proceeds
+// up in levels (height) until reaching the root. At each level, it calculates
+// which of the previous 2^(lvl-1) nodes is it's "parent", and if it should use
+// the high or low value of the parent (max or min of high/low). Nodes in the
+// first half of a particular level will always take the high value of their
+// parent, and nodes in the second half of their level will take the low value
+// of their parent.
+// Since k is in [1, 10^18], the max hight and therefore max recursion depth
+// should be about 60 (log2(1e18) = 59.79...).
 func solve(n, k int) (h, l int) {
-	// level := int(math.Ceil(math.Log2(float64(k+1)) - 1))
-	// height := level(k)
-	// // fmt.Println(level)
-	// h, l = n/2, (n-1)/2
-	// if height == 0 {
-	// 	return
-	// }
-
-	// // go right
-	// solve(max(h, l), k-above(height))
-
-	// // go left
-	// solve(min(h, l), k-above(height))
-
-	todepth := height(k)
-	curdepth := 0
-	visited := 0
-	for curdepth <= todepth {
-		if n%2 == 0 && n > 0 {
-			h = n / 2
-			l = (n - 1) / 2
-		} else {
-			h = n / 2
-			l = n / 2
-		}
-		// fmt.Println(h, l)
-
-		visited = above(curdepth)
-		curdepth++
-
-		// TODO: I don't have any idea how to figure out to "go right"
-		// or "go left". I think I'd have the problem solved if I could
-		// figure that out.
-		if (k-visited)%2 == 1 { // go right
-			// fmt.Println("went right")
-			n = max(h, l)
-			// k = k - 1
-		} else { // go left
-			// fmt.Println("went left")
-			n = min(h, l)
-			// k = k - 2
-		}
-		// fmt.Println(h, l, n, visited)
-	}
-
-	return
-}
-
-func solve3(n, k int) (h, l int) {
 	level := height(k)
 	if level <= 0 {
 		// base case
-		return high(n), low(n)
+		h, l = high(n), low(n)
+		// fmt.Printf("lv=%d (%d, %d) k=%d | ", level, h, l, k)
+		return
 	}
 
 	// some redudancy for clarity
-	lvlsq := pow2(level)
-	start := lvlsq
-	mid := lvlsq + lvlsq/2
+	twoPowLvl := pow2(level)
+	start := twoPowLvl
+	mid := twoPowLvl + twoPowLvl/2
 
 	// do "branch" here
 	var offset int
@@ -131,18 +62,31 @@ func solve3(n, k int) (h, l int) {
 		f = min
 	}
 
+	// for debugging math.Log2 error. see height() for explanation.
+	// if offset < 0 {
+	// 	panic(fmt.Errorf("offset <0: offset=%d\nlevel=%d k=%d\nstart=%d mid=%d\ntwoPowLvl=%d",
+	// 		offset, level, k, start, mid, twoPowLvl))
+	// }
+
 	// recurse to previous "level", get it's high and low,
 	// and then choose the max or min (as f) as the n used to calculate
 	// k's high and low
 	kPrev := pow2(level-1) + offset
-	nPrev := f(solve3(n, kPrev))
+	nPrev := f(solve(n, kPrev))
 
-	return high(nPrev), low(nPrev)
+	h, l = high(nPrev), low(nPrev)
+	// fmt.Printf("lv=%d (%d, %d) k=%d kP=%d | ", level, h, l, k, kPrev)
+	return
 }
 
 // height of kth (1,2,...) stall taken
 func height(k int) int {
-	return int(math.Floor(math.Log2(float64(k))))
+	// CANNOT use math.Log2 function. Using floats will give the wrong answer
+	// for 288230376151711743 and probably others because the precision
+	// of float64 causes 57.99999999999999999499464608778919110696411496790292147213...
+	// to round up to 58.
+	// return int(math.Floor(math.Log2(float64(k))))
+	return log2Fast(k)
 }
 
 func max(a, b int) int {
@@ -163,14 +107,24 @@ func pow2(x int) int {
 	return 1 << uint(x)
 }
 
-func above(h int) int {
-	return pow2(h+1) - 1
-}
-
 func high(n int) int {
 	return n / 2
 }
 
 func low(n int) int {
 	return (n - 1) / 2
+}
+
+func log2Slow(x int) int {
+	i := 0
+	for ; x > 1; i++ {
+		x = x >> 1
+	}
+	return i
+}
+
+func log2Fast(x int) int {
+	// for integers, log2() is essentially finding the number of bits
+	// required to store the number
+	return bits.Len64(uint64(x)) - 1
 }
